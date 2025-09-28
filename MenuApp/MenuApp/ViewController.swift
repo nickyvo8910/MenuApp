da8775adf57874d87a727ee8d8b0a7588939c6c5
Nicky Vo <nickyvo8910@gmail.com>
2025-09-28 08:58:51 +0100

    Project Setup

---
+++ MenuApp/MenuApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MenuApp
+//
+//  Created by Nicky Vo on 28/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
